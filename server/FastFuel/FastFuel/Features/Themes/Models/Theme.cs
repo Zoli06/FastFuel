99edb3ff6dcf5f3bb99e7326f180dc8e04871d54
Noah.James Massman <massman.noah@gmail.com>
2026-01-27 17:50:53 +0100

    Why is Theme Red

---
+++ server/FastFuel/FastFuel/Features/Themes/Models/Theme.cs
@@ -0,0 +1,14 @@
+﻿using FastFuel.Features.Common;
+
+namespace FastFuel.Features.Themes.Models
+{
+    public class Theme : IIdentifiable
+    {
+        public uint Id { get; init; }       
+        public string Name { get; set; } = string.Empty;
+        public string Background { get; set; } = "#ffffff";
+        public string Footer { get; set; } = "#f0f0f0";
+        public string ButtonPrimary { get; set; } = "#007bff";
+        public string ButtonSecondary { get; set; } = "#6c757d";
+    }
+}
---
+++ server/FastFuel/FastFuel/Features/Users/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using FastFuel.Features.Common;
+using FastFuel.Features.Restaurants.Models;
+using FastFuel.Features.StationCategories.Models;
+using System;
+using System.Collections.Generic;
+using FastFuel.Features.Themes.Models;
+using FastFuel.Features.Orders.Models;
+
+namespace FastFuel.Features.Users.Models
+{
+    public class User : IIdentifiable
+    {
+        public uint Id { get; init; }       
+        public string Name { get; set; } = string.Empty;
+        public uint UserId { get; init; }    
+        public List<Order> Orders { get; init; } = [];
+        public Theme Theme { get; init; } = new Theme(); 
+    }
+}
