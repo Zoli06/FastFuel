1827f24af5f0c51cede91ba4200f44908505a598
Zoli06 <csaszar.zoltan24@gmail.com>
2026-02-01 02:21:28 +0100

    Generate ts types from openapi and add example api client call

--- client/fast-fuel/src/App.tsx
+++ client/fast-fuel/src/App.tsx
@@ -1,10 +1,15 @@
 import '@mantine/core/styles.css'
 import './App.css'
 import BrowseFoods from "./pages/BrowseFoods.tsx";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
     return (
-        <BrowseFoods/>
+        <QueryClientProvider client={queryClient}>
+            <BrowseFoods/>
+        </QueryClientProvider>
     )
 }
 
