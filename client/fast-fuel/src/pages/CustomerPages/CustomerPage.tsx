0d6d00662c30659e21f6fca7b689760e4534d105
Noah.James Massman <massman.noah@gmail.com>
2026-03-24 16:06:39 +0100

    MenuFixes

--- client/fast-fuel/src/pages/CustomerPages/CustomerPage.tsx
+++ client/fast-fuel/src/pages/CustomerPages/CustomerPage.tsx
@@ -1,5 +1,5 @@
 import { Footer } from '../../components/Footer/Footer';
-import { CustomerMenu } from '../../components/CustomerManager/Customer.tsx';
+import { CustomerMenu } from '../../components/CustomerMenu/Customer.tsx';
 import { Header } from '../../components/Header/Header.tsx';
 
 export const CustomerPage = () => {
