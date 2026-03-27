a438d0c34a456e6c75d6cf72bc6221ee80a76566
Zoli06 <csaszar.zoltan24@gmail.com>
2026-03-27 10:10:41 +0100

    Title now works with all user types

--- client/fast-fuel/src/pages/HomePage.tsx
+++ client/fast-fuel/src/pages/HomePage.tsx
@@ -4,15 +4,9 @@ import { HomeMenu } from '../components/HomeMenu/HomeMenu.tsx';
 import { Header } from '../components/Header/Header.tsx';
 import { myCurrentUserQueryOptions } from '../lib/api-client.ts';
 
-const panelTitles: Record<string, string> = {
-  customer: 'Customer Panel',
-  employee: 'Employee Panel',
-  admin: 'Admin Panel',
-};
-
 export const HomePage = () => {
   const { data: currentUser } = useSuspenseQuery(myCurrentUserQueryOptions());
-  const title = panelTitles[currentUser.userType.toLowerCase()] ?? 'Home';
+  const title = currentUser.userType + ' Panel';
 
   return (
     <>
