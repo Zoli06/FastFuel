5844a29725df141e1997e7b5aee5e3ede30fe5a5
Noah <134604467+Noi101@users.noreply.github.com>
2026-03-30 15:12:32 +0200

    RemovingHeader

--- client/fast-fuel/src/pages/RegistrationPage.tsx
+++ client/fast-fuel/src/pages/RegistrationPage.tsx
@@ -3,10 +3,7 @@ import { Register } from '../components/Registration/Registration.tsx';
 export const RegistrationPage = () => {
   return (
     <div>
-      <Header title={'Registration'} authButton={'Login'} />
-
       <Register />
     </div>
   );
-  return <Register />;
 };
