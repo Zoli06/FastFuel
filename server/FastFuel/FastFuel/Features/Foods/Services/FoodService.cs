a42f5fddf55c1dfac20a6744c413449696be4d49
Zoli06 <csaszar.zoltan24@gmail.com>
2026-04-20 03:33:51 +0200

    Add price validation

--- server/FastFuel/FastFuel/Features/Foods/Services/FoodService.cs
+++ server/FastFuel/FastFuel/Features/Foods/Services/FoodService.cs
@@ -1,6 +1,8 @@
 using FastFuel.Features.Common.DbContexts;
+using FastFuel.Features.Common.Exceptions.AppExceptions;
 using FastFuel.Features.Common.Interfaces;
 using FastFuel.Features.Common.Services;
+using FastFuel.Features.Common.Services.CrudOperations;
 using FastFuel.Features.Foods.DTOs;
 using FastFuel.Features.Foods.Entities;
 using Microsoft.EntityFrameworkCore;
@@ -11,4 +13,40 @@ public class FoodService(FastFuelDbContext dbContext, IMapper<Food, FoodRequestD
     : CrudService<Food, FoodRequestDto, FoodResponseDto>(dbContext, mapper)
 {
     protected override DbSet<Food> DbSet { get; } = dbContext.Foods;
+
+    protected override Create<Food, FoodRequestDto, FoodResponseDto> CreateOperation =>
+        new Create(DbContext, DbSet, Mapper);
+
+    protected override Update<Food, FoodRequestDto, FoodResponseDto> UpdateOperation =>
+        new Update(DbContext, DbSet, Mapper);
+
+    private class Create(
+        FastFuelDbContext dbContext,
+        DbSet<Food> dbSet,
+        IMapper<Food, FoodRequestDto, FoodResponseDto> mapper)
+        : Create<Food, FoodRequestDto, FoodResponseDto>(dbContext, dbSet, mapper)
+    {
+        protected override Task SaveEntityAsync(FoodRequestDto requestDto, Food entity, uint? userId = null,
+            CancellationToken cancellationToken = default)
+        {
+            return entity.Price < 0
+                ? throw new ValidationAppException("Price cannot be negative.")
+                : base.SaveEntityAsync(requestDto, entity, userId, cancellationToken);
+        }
+    }
+
+    private class Update(
+        FastFuelDbContext dbContext,
+        DbSet<Food> dbSet,
+        IMapper<Food, FoodRequestDto, FoodResponseDto> mapper)
+        : Update<Food, FoodRequestDto, FoodResponseDto>(dbContext, dbSet, mapper)
+    {
+        protected override Task SaveEntityAsync(uint id, FoodRequestDto requestDto, Food entity, uint? userId = null,
+            CancellationToken cancellationToken = default)
+        {
+            return entity.Price < 0
+                ? throw new ValidationAppException("Price cannot be negative.")
+                : base.SaveEntityAsync(id, requestDto, entity, userId, cancellationToken);
+        }
+    }
 }
--- server/FastFuel/FastFuel/Features/Menus/Services/MenuService.cs
+++ server/FastFuel/FastFuel/Features/Menus/Services/MenuService.cs
@@ -1,6 +1,8 @@
 using FastFuel.Features.Common.DbContexts;
+using FastFuel.Features.Common.Exceptions.AppExceptions;
 using FastFuel.Features.Common.Interfaces;
 using FastFuel.Features.Common.Services;
+using FastFuel.Features.Common.Services.CrudOperations;
 using FastFuel.Features.Menus.DTOs;
 using FastFuel.Features.Menus.Entities;
 using Microsoft.EntityFrameworkCore;
@@ -11,4 +13,40 @@ public class MenuService(FastFuelDbContext dbContext, IMapper<Menu, MenuRequestD
     : CrudService<Menu, MenuRequestDto, MenuResponseDto>(dbContext, mapper)
 {
     protected override DbSet<Menu> DbSet { get; } = dbContext.Menus;
+
+    protected override Create<Menu, MenuRequestDto, MenuResponseDto> CreateOperation =>
+        new Create(DbContext, DbSet, Mapper);
+
+    protected override Update<Menu, MenuRequestDto, MenuResponseDto> UpdateOperation =>
+        new Update(DbContext, DbSet, Mapper);
+
+    private class Create(
+        FastFuelDbContext dbContext,
+        DbSet<Menu> dbSet,
+        IMapper<Menu, MenuRequestDto, MenuResponseDto> mapper)
+        : Create<Menu, MenuRequestDto, MenuResponseDto>(dbContext, dbSet, mapper)
+    {
+        protected override Task SaveEntityAsync(MenuRequestDto requestDto, Menu entity, uint? userId = null,
+            CancellationToken cancellationToken = default)
+        {
+            return entity.Price < 0
+                ? throw new ValidationAppException("Price cannot be negative.")
+                : base.SaveEntityAsync(requestDto, entity, userId, cancellationToken);
+        }
+    }
+
+    private class Update(
+        FastFuelDbContext dbContext,
+        DbSet<Menu> dbSet,
+        IMapper<Menu, MenuRequestDto, MenuResponseDto> mapper)
+        : Update<Menu, MenuRequestDto, MenuResponseDto>(dbContext, dbSet, mapper)
+    {
+        protected override Task SaveEntityAsync(uint id, MenuRequestDto requestDto, Menu entity, uint? userId = null,
+            CancellationToken cancellationToken = default)
+        {
+            return entity.Price < 0
+                ? throw new ValidationAppException("Price cannot be negative.")
+                : base.SaveEntityAsync(id, requestDto, entity, userId, cancellationToken);
+        }
+    }
 }
