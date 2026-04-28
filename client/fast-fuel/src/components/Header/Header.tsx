d7949ab5ee6fceed97dc27aeaade4cc0c82a21b2
Noah.James Massman <massman.noah@gmail.com>
2026-04-28 13:10:04 +0200

    HeaderAdjustment

--- client/fast-fuel/src/components/Header/Header.tsx
+++ client/fast-fuel/src/components/Header/Header.tsx
@@ -53,11 +53,13 @@ export const Header = ({
           </Link>
         )}
       </Flex>
-      <Center>
+      <Center flex={1}>
         <Stack align="center" gap={0}>
-          <Text fz={{ base: '1.2rem', xs: '2rem' }}>{title}</Text>
+          <Text fz={{ base: '1em', xs: '1.2rem', sm: '1.5em' }} ta={'center'}>
+            {title}
+          </Text>
           {currentUser && (
-            <Text size="sm" c="dimmed">
+            <Text size="sm" c="dimmed" ta={'center'}>
               {currentUser.name} ({currentUser.userType.toLowerCase()})
             </Text>
           )}
